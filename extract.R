9a5c771edf6f83df605b114a517163a74bed25aa
Will Cornwell <wcornwell@gmail.com>
2021-04-27 18:22:49 +1000

    tracking

---
+++ extract.R
@@ -0,0 +1,31 @@
+library(pdftools)
+library(stringr)
+a<-pdftools::pdf_data("B377 vol. 2.pdf")
+
+aa<-a[251]
+
+
+get_species<-function(aa){
+   aa$laggy<-aa$y-lag(aa$y)
+   aa$laggy_lead<-lag(aa$laggy)
+   bb<-filter(aa,(laggy>=28&laggy<=29)|(laggy_lead>=28&laggy_lead<=29)) #take first two words after 
+   bb %>% group_by(y) %>% summarize(species=paste(text,collapse=" "))->cc
+   return(bb)
+}
+
+dat<-lapply(a,get_species)
+dat2<-bind_rows(setNames(dat, seq_along(dat)), .id = "id")
+
+
+
+get_dist<-function(aa){
+  dists<-filter(aa,text=="Distribution:")
+  filter(aa,y%in%dists$y) %>%
+  group_by(y) %>% summarize(species=paste(text,collapse=" "))->cc
+  return(cc)
+}
+
+xx<-bind_rows(lapply(a,get_dist))
+
+
+
